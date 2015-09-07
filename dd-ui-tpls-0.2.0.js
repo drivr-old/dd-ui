a30407d919ba04c7f7dccba4af32113fb8da4a3d
Juras Norkus <jn@drivr.com>
2015-09-07 15:28:35 +0300

    docs(main): updated, removed unused files

--- dd-ui-tpls-0.2.0.js
+++ dd-ui-tpls-0.2.0.js
@@ -2,7 +2,7 @@
  * dd-ui
  * http://clickataxi.github.io/dd-ui/
 
- * Version: 0.2.0 - 2015-08-18
+ * Version: 0.2.0 - 2015-09-07
  * License: MIT
  */
 angular.module("dd.ui", ["dd.ui.tpls", "dd.ui.busy-element","dd.ui.datetimepicker","dd.ui.validation.phone","dd.ui.validation.sameAs","dd.ui.validation"]);
@@ -47,86 +47,95 @@ angular.module('dd.ui.busy-element', [])
 }]);
 angular.module('dd.ui.datetimepicker', ['ui.bootstrap'])
 
-.directive('datetimepicker', ['$document', function ($document) {
-			return {
-				restrict : 'EA',
-				require : 'ngModel',
-				templateUrl : function (element, attrs) {
-					return attrs.templateUrl || 'template/datetimepicker/datetimepicker.html';
-				},
-				scope : {
-					ngModel : '=',
-					minuteStep : '=?',
-					showSpinners : '=?',
-					showMeridian : '=?',
-					ngDisabled : '=?',
-					dateDisabled : '&'
-				},
+.directive('datetimepicker', ['$document', function($document) {
+	return {
+		restrict: 'EA',
+		require: 'ngModel',
+		templateUrl: function(element, attrs) {
+			return attrs.templateUrl || 'template/datetimepicker/datetimepicker.html';
+		},
+		scope: {
+			ngModel: '=',
+			minuteStep: '=?',
+			showSpinners: '=?',
+			showMeridian: '=?',
+			ngDisabled: '=?',
+			dateDisabled: '&'
+		},
 
-				link : function (scope, element) {
-					var firstTimeAssign = true;
-					//var timePickerElement = angular.element(element.children()[0]).children()[0];
-					var timePickerElement = element.children().eq(0).children()[0];
+		link: function(scope, element, attrs, ctrl) {
+			var firstTimeAssign = true;
+			var timePickerElement = element.children().eq(0).children()[0];
 
-					scope.$watch('ngModel', function (newTime) {
-						// if a time element is focused, updating its model will cause hours/minutes to be formatted by padding with leading zeros
-						if (!timePickerElement.contains($document[0].activeElement)) {
-							if (newTime == null || newTime === '') { // if the newTime is not defined
-								if (firstTimeAssign) { // if it's the first time we assign the time value
-									// create a new default time where the hours, minutes, seconds and milliseconds are set to 0.
-									newTime = new Date();
-									newTime.setHours(0, 0, 0, 0);
-								} else { // just leave the time unchanged
-									return;
-								}
-							}
+			// hook up a view change listener to fire ng-change
+			ctrl.$viewChangeListeners.push(function() {
+				scope.$eval(attrs.ngChange);
+			});
 
-							// Update timepicker (watch on ng-model in timepicker does not use object equality),
-							// also if the ngModel was not a Date, convert it to date
-							newTime = new Date(newTime);
-
-							scope.time = newTime; // change the time
-							if (firstTimeAssign) {
-								firstTimeAssign = false;
-							}
+			scope.$watch('ngModel', function(newTime) {
+				// if a time element is focused, updating its model will cause hours/minutes to be formatted by padding with leading zeros
+				if (!timePickerElement.contains($document[0].activeElement)) {
+					if (newTime == null || newTime === '') { // if the newTime is not defined
+						if (firstTimeAssign) { // if it's the first time we assign the time value
+							// create a new default time where the hours, minutes, seconds and milliseconds are set to 0.
+							newTime = new Date();
+							newTime.setHours(0, 0, 0, 0);
+						} else { // just leave the time unchanged
+							return;
 						}
-					}, true);
+					}
 
-					scope.dateChange = function () {
-						var time = scope.time;
-						if (scope.ngModel) {
-							scope.ngModel.setHours(time.getHours(), time.getMinutes(), 0, 0);
-						}
-					};
+					// Update timepicker (watch on ng-model in timepicker does not use object equality),
+					// also if the ngModel was not a Date, convert it to date
+					newTime = new Date(newTime);
 
-					scope.timeChange = function () {
-						if (scope.ngModel && scope.time) {
-							if (!(scope.ngModel instanceof Date)) {
-								scope.ngModel = new Date(scope.ngModel);
-							}
-							scope.ngModel.setHours(scope.time.getHours(), scope.time.getMinutes(), 0, 0);
+					scope.time = newTime; // change the time
+					if (firstTimeAssign) {
+						if (!scope.ngModel) {
+							scope.ngModel = new Date(newTime);
 						}
-					};
 
-					scope.open = function ($event) {
-						$event.preventDefault();
-						$event.stopPropagation();
-						scope.opened = true;
-					};
+						firstTimeAssign = false;
+					}
+				}
+			}, true);
 
-					(function init() {
-						if (scope.minuteStep === undefined) {
-							scope.minuteStep = 1;
-						}
-						if (scope.showSpinners === undefined) {
-							scope.showSpinners = true;
-						}
-					})();
+			scope.dateChange = function() {
+				var time = scope.time;
+				if (scope.ngModel) {
+					scope.ngModel.setHours(time.getHours(), time.getMinutes(), 0, 0);
+					ctrl.$setViewValue(scope.ngModel);
 				}
 			};
-		}
-	]);
 
+			scope.timeChange = function() {
+				if (scope.ngModel && scope.time) {
+					if (!(scope.ngModel instanceof Date)) {
+						scope.ngModel = new Date(scope.ngModel);
+					}
+					var newValue = new Date(scope.ngModel);
+					newValue.setHours(scope.time.getHours(), scope.time.getMinutes(), 0, 0);
+					ctrl.$setViewValue(newValue);
+				}
+			};
+
+			scope.open = function($event) {
+				$event.preventDefault();
+				$event.stopPropagation();
+				scope.opened = true;
+			};
+
+			(function init() {
+				if (scope.minuteStep === undefined) {
+					scope.minuteStep = 1;
+				}
+				if (scope.showSpinners === undefined) {
+					scope.showSpinners = true;
+				}
+			})();
+		}
+	};
+}]);
 var PHONE_REGEXP = /^\+\d{10,14}$/;
 var PHONE_COUNTRY_CODE_REGEXP = /^\+\d{1,3}$/;
 var PHONE_WO_COUNTRY_CODE_REGEXP = /^\d{7,13}$/;
