ab87afe85644dbd8ad586a6e0727cae1232746d0
Andzej <Maciusovic>
2016-02-18 14:22:06 +0200

    feat(datetimepicker): update docs for datetimepicker

--- src/dd-datetimepicker/docs/demo.js
+++ src/dd-datetimepicker/docs/demo.js
@@ -1,5 +1,5 @@
 angular.module('dd.ui.demo').controller('DdDateTimePickerDemoCtrl', ['$scope', function ($scope) {
-	$scope.dateTime = new Date();
+	$scope.dateTime = new Date('2016-02-18T11:47:37.205Z');
 	$scope.dateTime2 = new Date();
 	
 	$scope.disabled = function (date, mode) {
@@ -10,6 +10,16 @@ angular.module('dd.ui.demo').controller('DdDateTimePickerDemoCtrl', ['$scope', f
     
     $scope.change = function() {
         console.log($scope.dateTime, 'changed');
+    };
+    
+    $scope.setToNow = function() {
+        console.log('setToNow');
+        $scope.dateTime = new Date();
+    };
+    
+    $scope.setToEmpty = function() {
+        console.log('setToEmpty');
+        $scope.dateTime = null;
     };
     
 }]);
