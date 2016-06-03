c666e4242afe6ce3ff4ec5726edc4a3d1e885edb
Andzej <am@drivr.com>
2016-06-03 12:53:37 +0300

    chore(release): v0.5.4

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,14 @@
 angular.module('dd.ui.demo', ['dd.ui', 'plunker', 'ngTouch', 'ngAnimate', 'ngMockE2E'], function($httpProvider){
   FastClick.attach(document.body);
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}).run(['$location', function($location){
+}).run(['$location', '$locale', function($location, $locale){
   //Allows us to navigate to the correct element on initialization
   if ($location.path() !== '' && $location.path() !== '/') {
     smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
-      location.replace(el.id);
+      location.replace('#' + el.id);
     });
   }
+  
+  $locale.DATETIME_FORMATS.FIRSTDAYOFWEEK = 1;
+  
 }]);
