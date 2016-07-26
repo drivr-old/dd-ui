3834a457ebba4a12a0f9f8ac0c48b189e6bb335b
Andzej <andzej.mac@gmail.com>
2016-07-26 12:09:10 +0300

    feat(validation): add support for non input elements binding

--- src/form-validation/tests/show-errors.spec.js
+++ src/form-validation/tests/show-errors.spec.js
@@ -16,75 +16,87 @@ describe('Directive show-errors tests.', function () {
         });
     });
 
-    it('init element', function () {
-        var element = initDirective($scope, $compile);
-        expect(element).toBeDefined();
+    describe('Init', function () {
+        it('should init basic element', function () {
+            var element = initDirective($scope, $compile);
+            expect(element).toBeDefined();
+        });
+
+        it('should init element with ng-model and name', function () {
+            var element = $compile('<form><div show-errors><div ng-model="model" name="name"></div></div></form>')($scope);
+
+            $scope.$digest();
+
+            expect(element).toBeDefined();
+        });
+
+        it('throw error if form-group do not have element with ng-model and name', function () {
+            var compile = $compile('<form><div class="form-group" show-errors></div></form>');
+
+            $scope.$digest();
+
+            expect(function () {
+                compile($scope);
+                $timeout.flush();
+            }).toThrowError();
+        });
+
+        it('should add form-fields-group on compile step', function () {
+            var element = $compile('<form><div show-errors><input name="name" /></div></form>')($scope);
+
+            $scope.$digest();
+
+            expect(element.find('[show-errors]').hasClass('form-fields-group')).toBeTruthy();
+        });
     });
 
-    it('add has-error class if input is invalid', function () {
+    describe('has-error class', function () {
+        it('add has-error class if input is invalid', function () {
 
-        $scope.input = null;
-        var element = initDirective($scope, $compile);
+            $scope.input = null;
+            var element = initDirective($scope, $compile);
 
-        element.find('input').trigger('blur');
+            element.find('input').trigger('blur');
 
-        expect(element.find('.form-group').hasClass('has-error')).toBeTruthy();
-    });
+            expect(element.find('.form-group').hasClass('has-error')).toBeTruthy();
+        });
 
-    it('remove has-error class if input is become valid', function () {
+        it('remove has-error class if input is become valid', function () {
 
-        $scope.input = null;
-        var element = initDirective($scope, $compile);
+            $scope.input = null;
+            var element = initDirective($scope, $compile);
 
-        element.find('input').trigger('blur');
-        $scope.input = 'value';
-        $scope.$digest();
-        element.find('input').trigger('blur');
+            element.find('input').trigger('blur');
+            $scope.input = 'value';
+            $scope.$digest();
+            element.find('input').trigger('blur');
 
-        expect(element.find('.form-group').hasClass('has-error')).toBeFalsy();
-    });
+            expect(element.find('.form-group').hasClass('has-error')).toBeFalsy();
+        });
 
-    it('add has-error on event show-errors-check-validity', function () {
+        it('add has-error on event show-errors-check-validity', function () {
 
-        $scope.input = null;
-        var element = initDirective($scope, $compile);
+            $scope.input = null;
+            var element = initDirective($scope, $compile);
 
-        $scope.$broadcast('form-show-errors-check-validity');
-        $scope.$digest();
+            $scope.$broadcast('form-show-errors-check-validity');
+            $scope.$digest();
 
-        expect(element.find('.form-group').hasClass('has-error')).toBeTruthy();
-    });
+            expect(element.find('.form-group').hasClass('has-error')).toBeTruthy();
+        });
 
-    it('remove has-error on event show-errors-reset', function () {
+        it('remove has-error on event show-errors-reset', function () {
 
-        $scope.input = null;
-        var element = initDirective($scope, $compile);
-        element.find('.form-group').addClass('has-error');
+            $scope.input = null;
+            var element = initDirective($scope, $compile);
+            element.find('.form-group').addClass('has-error');
 
-        $scope.$broadcast('form-show-errors-reset');
-        $scope.$digest();
-        $timeout.flush();
-
-        expect(element.find('.form-group').hasClass('has-error')).toBeFalsy();
-    });
-
-    it('should add form-fields-group on compile step', function () {
-        var element = $compile('<form><div show-errors><input name="name" /></div></form>')($scope);
-
-        $scope.$digest();
-
-        expect(element.find('[show-errors]').hasClass('form-fields-group')).toBeTruthy();
-    });
-
-    it('throw error if form-group do not have input', function () {
-        var compile = $compile('<form><div class="form-group" show-errors></div></form>');
-
-        $scope.$digest();
-
-        expect(function () {
-            compile($scope);
+            $scope.$broadcast('form-show-errors-reset');
+            $scope.$digest();
             $timeout.flush();
-        }).toThrowError();
+
+            expect(element.find('.form-group').hasClass('has-error')).toBeFalsy();
+        });
     });
 
     describe('Custom error toggle', function () {
