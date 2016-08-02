8b1c923e774aa4e94899008b975c72b693fbb450
varnastadeus <varnastadeus@gmail.com>
2016-08-02 15:26:25 +0300

    style(tslint): semi

--- src/form-actions/form-actions.js
+++ src/form-actions/form-actions.js
@@ -25,15 +25,17 @@
                     var container = angular.element(document.getElementsByClassName(attrs.parentContainer));
                     dirtyWatch = scope.$watch('form.$dirty', function (newValue) {
                         if (newValue) {
-                            container.addClass('form-actions-visible')
+                            container.addClass('form-actions-visible');
                         } else {
-                            container.removeClass('form-actions-visible')
+                            container.removeClass('form-actions-visible');
                         }
                     });
                 }
 
-                scope.$on('$destroy', function(){
-                    if (dirtyWatch) dirtyWatch();
+                scope.$on('$destroy', function () {
+                    if (dirtyWatch) {
+                        dirtyWatch();
+                    }
                 });
             },
             restrict: 'E'
