bc020136511029506baeffe123198266d10379b2
varnastadeus <varnastadeus@gmail.com>
2016-10-11 12:51:43 +0300

    chore(release): v0.9.7

--- dist/dd-ui-0.9.7.js
+++ dist/dd-ui-0.9.7.js
@@ -2,7 +2,7 @@
  * dd-ui
  * http://clickataxi.github.io/dd-ui/
 
- * Version: 0.10.0-SNAPSHOT - 2016-10-11
+ * Version: 0.9.7 - 2016-10-11
  * License: MIT
  */angular.module("dd.ui", ["dd.ui.arrow-key-nav","dd.ui.busy-element","dd.ui.conversion","dd.ui.core","dd.ui.data-list","dd.ui.datetimepicker","dd.ui.dd-datepicker","dd.ui.dd-datetimepicker","dd.ui.dd-table","dd.ui.dd-timepicker","dd.ui.filter-field-focus","dd.ui.filter-helper","dd.ui.filter-tags","dd.ui.form-actions","dd.ui.form-validation","dd.ui.lookup","dd.ui.validation.phone","dd.ui.validation.sameAs","dd.ui.validation"]);
 angular.module('dd.ui.arrow-key-nav', [])
